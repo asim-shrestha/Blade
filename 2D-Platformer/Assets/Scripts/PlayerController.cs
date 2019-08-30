20d3c12c0a20cb29a8a7242d25f5c2c653110b33
Asim Shrestha <asim.shrestha@hotmail.com>
2019-08-29 20:29:33 -0700

    Started work on player controller

---
+++ 2D-Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] float playerSpeed;
+	[SerializeField] float jumpSpeed;
+	[SerializeField] Vector2 movementVector;
+	[SerializeField] bool isGrounded;
+	[SerializeField] bool isCrouching;
+
+	public Rigidbody2D rigidbody;
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//Find what direction the movement is taking place based on input
+		movementVector = new Vector2(Input.GetAxis("Horizontal"), 0f);
+		//CheckGrounded();
+		//Check for jumps
+		Jump();
+    }
+
+	//Runs every physics step
+	void FixedUpdate() {
+		//Move our character
+		MovePlayer(movementVector);
+	}
+
+	private void MovePlayer(Vector2 direction) {
+		transform.position += (Vector3)direction * playerSpeed * Time.deltaTime;
+		//rigidbody.MovePosition((Vector2)transform.position + (direction * playerSpeed * Time.deltaTime));
+	}
+
+	private void Jump() {
+		if (Input.GetButtonDown("Jump")) {
+			CheckGrounded();
+			rigidbody.velocity += (new Vector2(0f, 1f) * jumpSpeed);
+		}
+	}
+
+	private bool CheckGrounded() {
+		//Distance between object pivot and the bottom of the object
+		float distToGround = GetComponent<BoxCollider2D>().bounds.extents.y;
+
+		//Cast ray
+		Debug.Log("test");
+		Debug.Log(Physics2D.Raycast((Vector2)transform.position, Vector2.down, distToGround + 5f).collider.name);
+		return isGrounded;
+	}
+}
---
+++ 2D-Platformer/Assets/UnityVS/Editor/EditorFix.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+
+//#if ENABLE_VSTU //not defined in VS for me for some reason
+using SyntaxTree.VisualStudio.Unity.Bridge;
+
+[InitializeOnLoad]
+public class ProjectFileHook {
+	private const string SCHEMA = @"http://schemas.microsoft.com/developer/msbuild/2003";
+	private const string ASSEMBLY_NAME = "AssemblyName";//XML category for assembly
+	private const string ASSEMBLY_CSHARP_RUNTIME = "Assembly-CSharp";//value for assembly name
+	private const string TARGET_FRAMEWORK_VERSION = "TargetFrameworkVersion";//XML category for .net framework
+	private const string FRAMEWORK_EXPECTED = "v4.7.1";//old value for framework
+	private const string FRAMEWORK_DESIRED = "v4.6.1";//new value for framework
+
+	class Utf8StringWriter : StringWriter {//to allow UTF8 saving
+		public override Encoding Encoding {
+			get { return Encoding.UTF8; }
+		}
+	}
+
+	static ProjectFileHook() {
+		ProjectFilesGenerator.ProjectFileGeneration += (string name, string content) => {
+			var document = XDocument.Parse(content);
+			var assemblyName = document.Descendants(XName.Get(ASSEMBLY_NAME, SCHEMA)).FirstOrDefault();
+			if (null != assemblyName && assemblyName.Value.Contains(ASSEMBLY_CSHARP_RUNTIME)) {
+				var target = document.Descendants(XName.Get(TARGET_FRAMEWORK_VERSION, SCHEMA)).FirstOrDefault();
+				if (null != target && target.Value.Contains(FRAMEWORK_EXPECTED)) {
+					target.SetValue(FRAMEWORK_DESIRED);
+				}
+			}
+			var str = new Utf8StringWriter();
+			document.Save(str);
+
+			return str.ToString();
+		};
+	}
+}
+//#endif
