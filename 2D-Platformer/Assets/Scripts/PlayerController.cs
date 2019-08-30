3d3361e959f8a0d3830048c272b7e5039ed36813
Asim Shrestha <asim.shrestha@hotmail.com>
2019-08-30 15:18:28 -0700

    Added double jump

--- 2D-Platformer/Assets/Scripts/PlayerController.cs
+++ 2D-Platformer/Assets/Scripts/PlayerController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	[Header("Player configuration")]
 	[SerializeField] float playerSpeed = 12;
-	[SerializeField] float wallJumpSpeed ;
 	[SerializeField] float jumpSpeed = 15;
-	[SerializeField] Vector2 movementVector;
+	[SerializeField] float wallJumpSpeed = 5;
+	[SerializeField] float movementDirection;
+	[SerializeField] int maxJumps = 2;
 
+	[Header("Player States")]
+	[SerializeField] int jumpCount = 0;
+	[SerializeField] bool isGrounded = true;
 	[SerializeField] bool isCrouching = false;
 
 	private Rigidbody2D rb;
@@ -16,9 +21,10 @@ public class PlayerController : MonoBehaviour {
 
 	// Start is called before the first frame update
 	void Start() {
+		//Get rigid body component
 		rb = this.GetComponent<Rigidbody2D>();
 
-		//Get layermask so ray does not intersect with player
+		//Get layermask so ray casts do not intersect with the player layer
 		int groundLayer = 9;
 		groundLayerMask = 1 << groundLayer;
 	}
@@ -26,10 +32,13 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 		//Find what direction the movement is taking place based on input
-		movementVector = new Vector2(Input.GetAxis("Horizontal"), 0f);
+		movementDirection = Input.GetAxis("Horizontal");
+
+		//Check if the player is grounded
+		CheckGrounded();
 
 		//Check for jumps
-		Jump();
+		HandleJump();
 
 		//Check for wall jumps
 		//WallJump();
@@ -38,24 +47,25 @@ public class PlayerController : MonoBehaviour {
 	//Runs every physics step
 	void FixedUpdate() {
 		//Move our character
-		MovePlayer(movementVector);
+		MovePlayer();
 	}
 
-	private void MovePlayer(Vector2 direction) {
-		transform.position += (Vector3)direction * playerSpeed * Time.deltaTime;
-		//rigidbody.MovePosition((Vector2)transform.position + (direction * playerSpeed * Time.deltaTime));
+	private void MovePlayer() {
+		rb.velocity = new Vector2(movementDirection * playerSpeed, rb.velocity.y);
 	}
 
-	private void Jump() {
-		//Check if the jump button's been pressed and if the player is grounded 
-		if (Input.GetButtonDown("Jump") && CheckGrounded()) {
-			rb.velocity += (new Vector2(0f, 1f) * jumpSpeed);
+	private void HandleJump() {
+		//Check if the jump button's been pressed and if the player is grounded
+		//Also check to see if the player has reached the max jump count
+		if (Input.GetButtonDown("Jump") && (isGrounded || jumpCount != maxJumps)) {
+			jumpCount++;
+			rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
 		}
 	}
 
 	private void WallJump() {
 		//Check if the jump button's been pressed and if the player is grounded 
-		if (Input.GetButtonDown("Jump") && !CheckGrounded()) {
+		if (Input.GetButtonDown("Jump") && !isGrounded) {
 			//Check what wall we are sliding on
 			//-1 indicates a right wall, 0 indicates no wall, 1 indicates a left wall
 			int wallPosition = FindWallSlide();
@@ -66,17 +76,23 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
-	private bool CheckGrounded() {
+	//Use raycasting to figure out if the player is standing on the ground
+	private void CheckGrounded() {
 		//Distance between player pivot and the bottom of the player
 		float distFromGround = GetComponent<BoxCollider2D>().bounds.extents.y + 0.1f;   //Needs a little offset so the ray actually hits ground
 
 		//Cast ray and check if we found an object
 		if (Physics2D.Raycast(transform.position, Vector2.down, distFromGround, groundLayerMask).collider != null) {
-			return true;
+			isGrounded = true;
+			jumpCount = 0;
+			return;
 		}
 
 		//Ray didn't intersect so the player is not grounded
-		return false;
+		isGrounded = false;
+		//Make sure the player has 1 less jump avaliable
+		if(jumpCount == 0) { jumpCount++; }
+		return;
 	}
 
 	private int FindWallSlide() {
